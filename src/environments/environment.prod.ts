2c7fdc37e4f2991490d449f3d31f7435aa7876f3
Dinu Barbu <dinu@DevPlants-MacBook-Pro.local>
2021-01-30 01:37:31 +0200

    allow redirects in production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   firebase: {
     apiKey: 'AIzaSyB62TN1GJ7WHp9gLtijOK4HzgRXphy0T8U',
-    authDomain: 'dvn-firestarter.firebaseapp.com',
+    authDomain: 'orgazen.herokuapp.com',
     databaseURL: 'https://dvn-firestarter.firebaseio.com',
     projectId: 'dvn-firestarter',
     storageBucket: 'dvn-firestarter.appspot.com',
